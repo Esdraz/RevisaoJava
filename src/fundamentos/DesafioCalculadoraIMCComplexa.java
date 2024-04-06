26100cf3122aa382e52713216f7feeacc39a1b9f
Francisco <fsousa.jpa@gmail.com>
2024-04-06 11:00:33 -0300

    update: calculadoraIMC completa

--- src/fundamentos/DesafioCalculadoraIMCComplexa.java
+++ src/fundamentos/DesafioCalculadoraIMCComplexa.java
@@ -14,12 +14,23 @@ public class DesafioCalculadoraIMCComplexa {
         double altura = input.nextDouble();
         input.nextLine();
 
-        System.out.print("Digite o seu sexo: ");
+        System.out.print("Digite o seu sexo: (M/F): ");
         String sexo = input.nextLine();
 
-        double resultado = peso / (altura * altura);
+        double IMC = peso / (altura * altura);
+        System.out.printf("Seu IMC é: %.2f\n", IMC);
 
-        System.out.printf("%.2f", resultado);
+        if (IMC < 19.1 && sexo.equals("F") || IMC < 20.7 && sexo.equals("M")) {
+            System.out.println("Abaixo do peso!");
+        } else if (IMC <= 25.8 && sexo.equals("F") || IMC <= 26.4 && sexo.equals("M")) {
+            System.out.println("No peso ideal!");
+        } else if (IMC <= 27.3 && sexo.equals("F") || IMC <= 27.8 && sexo.equals("M")) {
+            System.out.println("Um pouco acima do peso!");
+        } else if (IMC <= 32.3 && sexo.equals("F") || IMC <= 31.1 && sexo.equals("M")) {
+            System.out.println("Acima do peso ideal!");
+        } else {
+            System.out.println("Obeso!");
+        }
 
         input.close();
     }
